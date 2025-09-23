1d151aff898b352691671fd9bab6c4084d68302b
LAREDO LANCER <lancerajayi@yahoo.com>
2025-09-23 11:50:09 +0100

    Deploy App 12

--- INFRA/provider.tf
+++ INFRA/provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-#   subscription_id = "ff4b0f27-bd73-4dbf-a413-78b31e6d283e"
-#   tenant_id       = "cd1f86d6-1102-4c2a-b885-60cc94cf9e23"
+     subscription_id = "ff4b0f27-bd73-4dbf-a413-78b31e6d283e"
+     tenant_id       = "cd1f86d6-1102-4c2a-b885-60cc94cf9e23"
 }
