73e020b3be9ae257d908ee5e35278a0159eee172
LAREDO LANCER <lancerajayi@yahoo.com>
2025-10-11 00:04:49 +0100

    Testing Deployment 3

--- INFRA/backend.tf
+++ INFRA/backend.tf
@@ -1,9 +1,21 @@
 
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "aks-resource-group"
+#     storage_account_name = "k8scloudtfstateuks"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
+
 terraform {
   backend "azurerm" {
-    resource_group_name  = "aks-resource-group"
-    storage_account_name = "k8scloudtfstateuks"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+    access_key           = ""
   }
 }
+
+# To use this backend configuration, create a file named `backend.tfvars` in the same directory
