e69d0cce417da49c387daa7b6e57da8054e02049
LAREDO LANCER <lancerajayi@yahoo.com>
2025-10-11 03:24:58 +0100

    Testing Deployment 27

--- INFRA/output.tf
+++ INFRA/output.tf
@@ -1,35 +1,44 @@
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-  sensitive = true
-}
+# output "kube_config" {
+#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+#   sensitive = true
+# }
 
-output "kube_config_client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
-  sensitive = true
-}
+# output "kube_config_client_certificate" {
+#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+#   sensitive = true
+# }
 
-output "kube_config_client_key" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
-  sensitive = true
-}
+# output "kube_config_client_key" {
+#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
+#   sensitive = true
+# }
 
-output "kube_config_cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
-  sensitive = true
-}
+# output "kube_config_cluster_ca_certificate" {
+#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
+#   sensitive = true
+# }
 
-output "kube_config_host" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  sensitive = true
+# output "kube_config_host" {
+#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+#   sensitive = true
+# }
+
+# output "resource_group_name" {
+#   description = "The name of the Azure Resource Group"
+#   value       = azurerm_resource_group.rg.name
+# }
+
+# output "aks_name" {
+#   description = "The name of the AKS Cluster"
+#   value       = azurerm_kubernetes_cluster.aks.name
+# }
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.aks_rg.name
+  description = "The name of the Azure Resource Group for AKS"
 }
 
 output "aks_name" {
-  description = "Name of the AKS cluster"
   value       = azurerm_kubernetes_cluster.aks_cluster.name
+  description = "The name of the AKS cluster"
 }
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.aks_rg.name
-}
-
