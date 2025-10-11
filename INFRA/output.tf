450c99ab5f72be991a82eedf385bb3788c8093e4
LAREDO LANCER <lancerajayi@yahoo.com>
2025-10-11 14:14:28 +0100

    Testing Deployment 35

--- INFRA/output.tf
+++ INFRA/output.tf
@@ -1,7 +1,3 @@
-# output "kube_config" {
-#   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-#   sensitive = true
-# }
 
 output "kube_config_client_certificate" {
   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
